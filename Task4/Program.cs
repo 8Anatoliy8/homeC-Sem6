fa5ec6cf9bcb3eb3d1660f6d85a1cd545b6b93d0
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-01-31 22:17:12 +0300

    complete Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, 
+// в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string ReverseWords (string str)
+{
+    string [] words = str.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ", words);
+}
+
+Console.Write("Введите строку: ");
+string inputString = Console.ReadLine();
+string result = ReverseWords (inputString);
+Console.Write ($"Перевернутая строка: {result}");
