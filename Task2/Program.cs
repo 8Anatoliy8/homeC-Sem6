319f2de9c9a535c8983727e13541f2057f683d39
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-01-31 19:30:48 +0300

    Complete Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Задайте строку, 
+// содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.Write("Введите строку: ");
+string inputString = Console.ReadLine();
+inputString = inputString.ToLower();
+Console.Write ($"Ваша строка в нижнем регистре: {inputString}");
