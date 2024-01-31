f4f64168b009df90ef2c438aff91037e9b2871f5
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-01-31 20:42:21 +0300

    complete task3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+bool IsStringPalindrom(string str)
+{
+    str = str.ToLower();
+    for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+    {
+        if (str[i] != str[j])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.Write("Введите строку для проверки палидрома: ");
+string inputString = Console.ReadLine();
+
+if (IsStringPalindrom(inputString))
+{
+    Console.Write("Введенная строка является палидромом");
+}
+else
+{
+    Console.Write("Введенная строка не является палидромом");
+}
